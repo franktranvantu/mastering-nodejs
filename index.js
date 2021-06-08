748320bb063615305952dcc88ae37eeaf9849016
frank <franktranvantu@gmail.com>
2021-06-08 22:33:12 +0700

    Update

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ console.log('Before');
 getUser(1, user => {
     getRepositories(user.gitHubUsername, repositories => {
         getCommits(repositories[0], commits => {
-
+            console.log(commits);
         });
     });
 });
@@ -14,22 +14,24 @@ console.log('Before');
 const user = getUser(1);
 const repositories = getRepositories(user.gitHubUsername);
 const commits = getCommits(repositories[0]);
+console.log(commits);
 console.log('After');
 
 function getUser(id, callback) {
     setTimeout(() => {
-        console.log('Reading a use from the database...');
+        console.log(`Getting user from user id ${id}...`);
         callback({id: id, gitHubUsername: 'Frank'});
     }, 2000);
 }
 
 function getRepositories(username, callback) {
     setTimeout(() => {
-        console.log('Calling git hub api...');
+        console.log(`Getting repositories from username ${username}...`);
         callback(['repo1', 'repo2', 'repo3']);
     }, 2000);
 }
 
-function getCommits() {
-
+function getCommits(repository, callback) {
+    console.log(`Getting commits from repository ${repository}...`);
+    callback(['Commit 1', 'Commit 2', 'Commit 3']);
 }
