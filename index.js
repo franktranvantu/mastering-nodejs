0acaebe092f5fb667b3f58b6db64bbef5c281c2c
frank <franktranvantu@gmail.com>
2021-06-08 14:02:21 +0700

    Update

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.get('/api/courses/:id', (req, res) => {
   const id = parseInt(req.params.id);
   const course = courses.find(course => course.id === id);
   if (!course) {
-    res.send('Course not found');
+    return res.status(404).send('Course not found');
   }
   res.send(course);
 });
