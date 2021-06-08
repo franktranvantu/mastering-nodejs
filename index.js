4601477daaa8639a08eb0c94f1ba385ab6f47e32
frank <franktranvantu@gmail.com>
2021-06-08 18:27:57 +0700

    Update

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 console.log('Before');
 getUser(1, user => {
     console.log(user);
+    getRepositories(user.gitHubUsername, repositories => {
+        console.log(repositories);
+    });
 });
 console.log('After');
 
@@ -8,5 +11,12 @@ function getUser(id, callback) {
     setTimeout(() => {
         console.log('Reading a use from the database...');
         callback({id: id, gitHubUsername: 'Frank'});
+    }, 2000);
+}
+
+function getRepositories(username, callback) {
+    setTimeout(() => {
+        console.log('Calling git hub api...');
+        callback(['repo1', 'repo2', 'repo3']);
     }, 2000);
 }
