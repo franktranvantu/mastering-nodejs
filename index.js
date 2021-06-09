a99e9e489aca87e3d8f0c71855818d4cff347429
frank <franktranvantu@gmail.com>
2021-06-09 09:53:34 +0700

    Update

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-// Asynchronous
-console.log('Before');
 getUser(1, user => {
     getRepositories(user.gitHubUsername, repositories => {
         getCommits(repositories[0], commits => {
@@ -7,31 +5,28 @@ getUser(1, user => {
         });
     });
 });
-console.log('After');
 
-// Synchronous
-console.log('Before');
-const user = getUser(1);
-const repositories = getRepositories(user.gitHubUsername);
-const commits = getCommits(repositories[0]);
-console.log(commits);
-console.log('After');
-
-function getUser(id, callback) {
-    setTimeout(() => {
-        console.log(`Getting user from user id ${id}...`);
-        callback({id: id, gitHubUsername: 'Frank'});
-    }, 2000);
+function getUser(id) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log(`Getting user from user id ${id}...`);
+            resolve({id: id, gitHubUsername: 'Frank'});
+        }, 2000);
+    });
 }
 
-function getRepositories(username, callback) {
-    setTimeout(() => {
-        console.log(`Getting repositories from username ${username}...`);
-        callback(['repo1', 'repo2', 'repo3']);
-    }, 2000);
+function getRepositories(username) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log(`Getting repositories from username ${username}...`);
+            resolve(['repo1', 'repo2', 'repo3']);
+        }, 2000);
+    })
 }
 
-function getCommits(repository, callback) {
-    console.log(`Getting commits from repository ${repository}...`);
-    callback(['Commit 1', 'Commit 2', 'Commit 3']);
+function getCommits(repository) {
+    return new Promise((resolve, reject) => {
+        console.log(`Getting commits from repository ${repository}...`);
+        resolve(['Commit 1', 'Commit 2', 'Commit 3']);
+    })
 }
