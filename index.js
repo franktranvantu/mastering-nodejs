157106dc88ea5fdc5621b7fb3ef913da90ffdfd1
frank <franktranvantu@gmail.com>
2021-06-09 11:20:47 +0700

    Update

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/playground')
+mongoose.connect('mongodb://localhost:27017/playground')
     .then(() => console.log('Connected to MongoDB.'))
     .catch(error => console.error('Could not connect to MongoDB.', error));
