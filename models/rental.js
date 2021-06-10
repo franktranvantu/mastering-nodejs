e960674b03150de41290f8ff4664fbf9f5cf290d
frank <franktranvantu@gmail.com>
2021-06-10 15:09:11 +0700

    Update

--- models/rental.js
+++ models/rental.js
@@ -1,5 +1,4 @@
 const Joi = require('joi');
-Joi.objectId = require('joi-objectid')(Joi);
 const mongoose = require('mongoose');
 
 const Rental = mongoose.model('Rental', new mongoose.Schema({
@@ -57,12 +56,12 @@ const Rental = mongoose.model('Rental', new mongoose.Schema({
 }));
 
 function validateRental(rental) {
-  const schema = {
+  const schema = Joi.object({
     customerId: Joi.objectId().required(),
     movieId: Joi.objectId().required()
-  };
+  });
 
-  return Joi.validate(rental, schema);
+  return schema.validate(rental);
 }
 
 exports.Rental = Rental; 
