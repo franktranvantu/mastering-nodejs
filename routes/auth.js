1ff85c2ef6dd80bb00f42ccf01bc6eac324acea7
frank <franktranvantu@gmail.com>
2021-06-10 18:15:00 +0700

    Update

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,6 @@
 const express = require('express');
 const Joi = require('joi');
-const _ = require('lodash');
 const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-const config = require('config');
 const {User} = require('../models/user');
 
 const router = express.Router();
@@ -22,7 +19,7 @@ router.post('/', async (req, res) => {
     return res.status(400).send('Invalid email or password.');
   }
 
-  const token = jwt.sign({_id: user._id}, config.get('jwtPrivateKey'));
+  const token = user.generateAuthToken();
   res.send(token);
 });
 
--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const _ = require('lodash');
 const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-const config = require('config');
 const {User, validate} = require('../models/user');
 
 const router = express.Router();
@@ -21,7 +19,7 @@ router.post('/', async (req, res) => {
   user.password = await bcrypt.hash(user.password, salt);
   await user.save();
 
-  const token = jwt.sign({_id: user._id}, config.get('jwtPrivateKey'));
+  const token = user.generateAuthToken();
   res.header('x-auth-token', token).send(_.pick(user, ['_id', 'name', 'email']));
 });
 
