637d2d0e52b4f0395e42edea61c622d4a4ee4c11
frank <franktranvantu@gmail.com>
2021-06-11 12:53:19 +0700

    Update

--- README.md
+++ README.md
@@ -1,5 +1,1 @@
-# Writing Your First Unit Test
-
-```bash
-npm i jest --save-dev
-```
+# Testing Numbers
--- tests/lib.test.js
+++ tests/lib.test.js
@@ -1,7 +1,16 @@
-test('First test', () => {
+const {absolute} = require('../lib');
 
+test('absolute - should return a positive number if input is positive', () => {
+  const result = absolute(1);
+  expect(result).toBe(1);
 });
 
-test('Second test', () => {
-  throw new Error("Something failed.");
+test('absolute - should return a positive number if input is negative', () => {
+  const result = absolute(-1);
+  expect(result).toBe(1);
+});
+
+test('absolute - should return 0 if input is 0', () => {
+  const result = absolute(0);
+  expect(result).toBe(0);
 });
