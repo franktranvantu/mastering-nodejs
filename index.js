ed6d62b4536238d50a9de38cb73513dee00dd425
frank <franktranvantu@gmail.com>
2021-06-12 12:55:09 +0700

    Update

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require('express');
+const config = require('config');
+const Joi = require('joi');
+Joi.objectId = require('joi-objectid')(Joi);
 const app = express();
 require('./startup/logging')();
 require('./startup/routes')(app);
 require('./startup/db')();
 require('./startup/config')();
-require('./startup/validation')();
-const logger = require('./middleware/logger')
 
-const port = process.env.PORT || 3000;
-app.listen(port, () => logger.info(`Listening on port ${port}...`));
+const port = process.env.PORT || config.get('port');
+app.listen(port, () => console.log(`Listening on port ${port}...`));
