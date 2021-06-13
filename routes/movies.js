51936a9aedf53a4415254569a64efda48faba055
frank <franktranvantu@gmail.com>
2021-06-13 12:54:13 +0700

    Update

--- routes/movies.js
+++ routes/movies.js
@@ -2,7 +2,6 @@ const express = require('express');
 const {Genre} = require('../models/genre');
 const {Movie, validate} = require('../models/movie');
 const auth = require('../middleware/auth');
-const admin = require('../middleware/admin');
 
 const router = express.Router();
 
@@ -55,7 +54,7 @@ router.put('/:id', [auth], async (req, res) => {
   res.send(movie);
 });
 
-router.delete('/:id', [auth, admin], async (req, res) => {
+router.delete('/:id', [auth], async (req, res) => {
   const movie = await Movie.findByIdAndRemove(req.params.id);
 
   if (!movie) return res.status(404).send('The movie with the given ID was not found.');
